9614424bfac65c167a1f73ea4dd2d98731288bb6
Julie Dressner <jmdressner@gmail.com>
2018-05-29 19:47:28 -0500

    updating project

--- clock_radio/clock_radio/Program.cs
+++ clock_radio/clock_radio/Program.cs
@@ -10,6 +10,16 @@ namespace clock_radio
     {
         static void Main(string[] args)
         {
+            ClockRadio myRadio = new ClockRadio();
+            myRadio.GetTime();
+            //Console.WriteLine(myRadio.time);
+            Console.ReadLine();
+            myRadio.SetAlarm();
+            //Console.WriteLine(myRadio.alarm);
+            Console.ReadLine();
+            myRadio.SetRadioStation();
+            //Console.WriteLine(myRadio.radioStation);
+            Console.ReadLine();
         }
     }
 }
--- clock_radio/clock_radio/clockradio.cs
+++ clock_radio/clock_radio/clockradio.cs
@@ -6,7 +6,59 @@ using System.Threading.Tasks;
 
 namespace clock_radio
 {
-    class clockradio
+    class ClockRadio
     {
+        // member variables
+
+        public string time;
+        public string alarm;
+        public string setTime;
+        public string radioStation;
+        public string setRadioStation;
+
+        // constructor
+
+        public ClockRadio()
+        {
+
+        }
+
+        // member methods
+
+        public string GetTime()
+        {
+            Console.WriteLine("Please enter the time.");
+            time = Console.ReadLine();
+            return time;
+           
+        }
+       
+        public string SetAlarm()
+        {
+            Console.WriteLine("Would you like to set an alarm? Please enter 'Yes' or 'No'");
+            alarm = Console.ReadLine().ToLower();
+            if (alarm == "yes")
+            {
+               Console.WriteLine("What time would you like to set your alarm?");
+                setTime = Console.ReadLine();
+                return setTime;
+            }
+            return alarm;
+        }
+
+        public string SetRadioStation()
+        {
+            Console.WriteLine("Would you like to change the Radio Station? Please enter 'Yes' or 'No'");
+            radioStation = Console.ReadLine().ToLower();
+            if (radioStation == "yes")
+            {
+                Console.WriteLine("What station would you like?");
+                setRadioStation = Console.ReadLine();
+                return setRadioStation;
+            }
+            return radioStation;
+        }
+
+
     }
 }
